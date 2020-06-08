b6286029e3ed4f26616fb951b794b9779002c8b3
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-08 14:25:40 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,23 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+if speed < 40
+  return true
+  elsif speed > 60
+  return true
+else
+  return false 
+end
 end
 
 
 
 def not_safe?(speed)
-	
+  
+	speed < 60 ? false : true
+	speed > 40 ? false : true 
+	speed < 40 ? true : false
+	speed > 60 ? true : false
 end
 	
 
